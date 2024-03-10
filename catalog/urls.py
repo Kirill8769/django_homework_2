85f52342f1c8cc8222f93707f94e21df1fdfdc5c
kirill8769 <tkkrozn@gmail.com>
2024-03-10 18:13:50 +0300

    edit urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from catalog import views
+from catalog import views, apps
+
+app_name = apps.CatalogConfig.name
 
 urlpatterns = [
-    path("", views.index),
-    path("contacts/", views.contacts),
+    path("", views.index, name="index"),
+    path("contacts/", views.contacts, name="contacts"),
 ]
