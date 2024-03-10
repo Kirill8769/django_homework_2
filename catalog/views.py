ed88d31293747678f8b2e7d23b495190a6a81602
kirill8769 <tkkrozn@gmail.com>
2024-03-10 18:15:33 +0300

    rename home to index

--- catalog/views.py
+++ catalog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'catalog/home.html')
+    return render(request, 'catalog/index.html')
 
 
 def contacts(request):
